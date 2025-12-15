c15f52322e18d80f16399c1312f6c52928681337
Julien Bernard <julien.bernard@univ-fcomte.fr>
2025-12-15 12:13:33 +0100

    remove fmt from deps

--- packages/s/sched/xmake.lua
+++ packages/s/sched/xmake.lua
@@ -1,13 +1,13 @@
 package("sched")
     set_homepage("https://github.com/SchedulingLab/libsched")
-    set_description("C++17 library for playing with scheduling problems")
+    set_description("C++23 library for playing with scheduling problems")
     set_license("GPL-3.0")
 
     set_urls("https://github.com/SchedulingLab/libsched.git")
 
     -- add_versions("2024.02.07", "71ff2b7bbda3568f820681b1bec52c2a911c0723")
 
-    add_deps("lqp", "fmt", "nlohmann_json")
+    add_deps("lqp", "nlohmann_json")
 
     on_install("windows", "linux", function (package)
         import("package.tools.xmake").install(package)
@@ -20,5 +20,5 @@ package("sched")
               sched::Log::println("Hello World!");
               return 0;
             }
-        ]]}, {configs = {languages = "cxx17"}}))
+        ]]}, {configs = {languages = "cxx23"}}))
     end)
